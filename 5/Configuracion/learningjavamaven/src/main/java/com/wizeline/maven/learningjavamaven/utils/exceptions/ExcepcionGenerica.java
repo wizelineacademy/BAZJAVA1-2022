854e4a33981e287f4f7aa45c2a05aaf30aeb55a9
3010 - Jonathan Torres <jonathan.torres@wizeline.com>
2022-09-23 13:34:01 -0500

    Change name to LearningJava

--- 5/Configuracion/learningjavamaven/src/main/java/com/wizeline/maven/learningjavamaven/utils/exceptions/ExcepcionGenerica.java
+++
@@ -1,7 +0,0 @@
-package com.wizeline.maven.learningjavamaven.utils.exceptions;
-
-public class ExcepcionGenerica extends RuntimeException {
-    public ExcepcionGenerica(String mensajeError) {
-        super(mensajeError);
-    }
-}
